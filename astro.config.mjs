9115de3bd8c81faba2527d133bd724ea5eb3dd75
oriolss1996 <oriolss96@gmail.com>
2026-04-16 16:12:10 +0200

    configurando keystatic

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,21 +2,28 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
-
 import react from "@astrojs/react";
 import keystatic from "@keystatic/astro";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://durummixto.com",
+    site: "https://durummixto.com",
     output: 'server',
-	integrations: [mdx(), sitemap(), react(), keystatic()],
-	adapter: cloudflare({
+    integrations: [mdx(), sitemap(), react(), keystatic()],
+    adapter: cloudflare({
         imageService: 'passthrough',
         platformProxy: {
             enabled: true,
         },
-	}),
+    }),
+    // ESTO ES LO QUE SOLUCIONA EL ERROR DE LA CONSOLA (Pantalla en blanco)
+    vite: {
+        optimizeDeps: {
+            exclude: ['@keystatic/core', '@keystatic/astro']
+        },
+        ssr: {
+            noExternal: ['@keystatic/core', '@keystatic/astro']
+        }
+    }
 });
